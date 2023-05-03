4d8c48395511f5e401309c18a827c62aea8ce46a
fannanqi <105165865+fannanqi@users.noreply.github.com>
2023-05-03 22:02:33 +0800

    Update Contrller.java

--- src/main/java/com/example/work8/controller/Contrller.java
+++ src/main/java/com/example/work8/controller/Contrller.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package com.example.work8.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
